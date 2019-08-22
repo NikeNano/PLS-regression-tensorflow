2327f5ede4969485c81c799cec856460540cd2a4
Jaime Ferrando Huertas <jaime.ferrando_huertas@sandvik.com>
2019-08-22 18:26:21 +0000

    Implemented tf.function autograph generation

--- pls.py
+++ pls.py
@@ -1,14 +1,17 @@
 import tensorflow as tf
 
 @tf.function
-def _nipals_tensorflow(X,Y, max_iter=500, tol=1e-06):
+def _nipals_tensorflow(X,Y, max_iter=500, tol=1e-06,norm_y_weights=False):
+    norm_y_weights = tf.convert_to_tensor(norm_y_weights)
     y_score = Y
-    x_weights_old = 0
+    x_weights_old = tf.constant(0,shape=[X.shape[1],1],dtype='float64')
     ite = 1
     X_pinv = Y_pinv = None
     eps = tf.keras.backend.epsilon()
     # Inner loop of the Wold algo.
-    while True:
+    x_weights = tf.constant(0,shape=[X.shape[1],1],dtype='float64')
+    y_weights = tf.constant(0,shape=[1,1],dtype='float64')
+    while tf.constant(True):
         #1. Regress into X
         x_weights = tf.matmul(tf.transpose(X),y_score)/(tf.matmul(tf.transpose(y_score),y_score))
         # 1.2 Normalize u
@@ -19,17 +22,22 @@ def _nipals_tensorflow(X,Y, max_iter=500, tol=1e-06):
         #2. Regress into Y
         y_weights = tf.matmul(tf.transpose(Y),x_score)/(tf.matmul(tf.transpose(x_score),x_score))
         # 2.1 Normalize y_weights
-        y_weights /= y_weights/tf.math.sqrt(tf.matmul(tf.transpose(y_weights),y_weights))
+        if(norm_y_weights):
+            y_weights /= y_weights/tf.math.sqrt(tf.matmul(tf.transpose(y_weights),y_weights))
         # 2.2 Update y_score: the Y latent scores
         y_score = tf.matmul(Y,y_weights)/tf.matmul(tf.transpose(y_weights),y_weights)
         x_weights_diff = tf.math.subtract(x_weights,x_weights_old)
-        if tf.matmul(tf.transpose(x_weights_diff),x_weights_diff) < tol or Y.shape[1] ==1:
+        if tf.matmul(tf.transpose(x_weights_diff),x_weights_diff)[0][0] < tol:# or Y.shape[1] ==1:
             break
-        if ite == max_iter:
+        if ite == 1:
             warnings.warn('Maximum number of iterations reached',
                           ConvergenceWarning)
-            break
+        break
         x_weights_old = x_weights
         ite += 1
     return x_weights, y_weights, ite
+
+
+
+
 
