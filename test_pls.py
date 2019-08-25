0d4e9113700c2b72c2f04fe194360d4243a4e2b8
NikeNano <niklas.sven.hansson@gmail.com>
2019-08-25 21:50:21 +0200

    updated some tests

--- test_pls.py
+++ test_pls.py
@@ -11,10 +11,12 @@ def test__nipals_tensorflow():
     iris = datasets.load_iris()
     X = check_array(iris.data, dtype=np.float64,ensure_min_samples=2)
     Y = check_array(iris.target, dtype=np.float64,ensure_2d=False)
+
     if Y.ndim == 1:
             Y = Y.reshape(-1, 1)
     Xt = tf.convert_to_tensor(X)
     Yt = tf.convert_to_tensor(Y)
+    Xt2 = tf.Variable(Xt)
 
     x_weights, y_weights, ite = _nipals_tensorflow(X=Xt,Y=Yt,max_iter=500, tol=1e-06,norm_y_weights=False)
     assert ite.numpy() == 1, "Should only need one itteration since one dependent variable"
@@ -24,4 +26,7 @@ def test__nipals_tensorflow():
     assert x_weights.shape[0] == X.shape[1], "Wrong output shape of X"
     assert y_weights.shape[0] == Y.shape[1], "Wrong output shape of Y"
     assert y_weights.shape[0] == 1, "Wrong output shape of Y"
+    assert Xt.shape == Xt2.shape
+    assert Xt != Xt2
+
 
